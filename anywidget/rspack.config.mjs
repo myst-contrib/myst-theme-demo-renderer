6d74dbaf58db26f5d99656d490cfb1a9bb5c892f
Angus Hollands <goosey15@gmail.com>
2026-07-02 17:59:20 +0100

    wip: initial demo

---
+++ anywidget/rspack.config.mjs
@@ -0,0 +1,85 @@
+// @ts-check
+import path from "node:path";
+import { defineConfig } from "@rspack/cli";
+import { rspack } from "@rspack/core";
+import { ReactRefreshRspackPlugin } from "@rspack/plugin-react-refresh";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const themeSharedPackages = [
+  "@myst-theme/anywidget",
+  "@myst-theme/common",
+  "@myst-theme/diagrams",
+  "@myst-theme/frontmatter",
+  "@myst-theme/icons",
+  "@myst-theme/jupyter",
+  "@myst-theme/landing-pages",
+  "myst-demo",
+  "myst-to-react",
+  "@myst-theme/providers",
+  "@myst-theme/search-minisearch",
+  "@myst-theme/search",
+  "@myst-theme/site",
+  "react",
+];
+
+/** @type {import('@rspack/core').Configuration} */
+
+export default defineConfig({
+  entry: "./src/index.js",
+  context: import.meta.dirname,
+  output: {
+    // set uniqueName explicitly to make react-refresh works
+    uniqueName: "custom_anywidget_renderer",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs|jsx)$/,
+        include: path.resolve(import.meta.dirname, "src"),
+        use: {
+          loader: "builtin:swc-loader",
+          options: {
+            detectSyntax: "auto",
+            jsc: {
+              transform: {
+                react: {
+                  runtime: "automatic",
+                  refresh: !isProduction,
+                },
+              },
+            },
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // exclude container entry from html, to use the correct HMR handler
+      excludeChunks: ["custom_anywidget_renderer"],
+    }),
+    new rspack.container.ModuleFederationPlugin({
+      // A unique name
+      name: "custom_anywidget_renderer",
+      // List of exposed modules
+      exposes: {
+        ".": "./src/index",
+      },
+
+      // list of shared modules
+      shared: themeSharedPackages.map((pkg) => {
+        return { [pkg]: { import: false, singleton: true } };
+      }),
+    }),
+    !isProduction && new ReactRefreshRspackPlugin(),
+  ],
+  devServer: {
+    port: 8081,
+    // add CORS header for HMR chunk (xxx.hot-update.json and xxx.hot-update.js)
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+});
---
+++ demo/main.md
@@ -0,0 +1,13 @@
+# Demo
+
+::::{div .blue-border}
+
+:::{list-table}
+- - foo
+  - bar
+- - baz
+  - bat
+:::
+
+Nice table! It's _AST_ under the **hood**
+::::
